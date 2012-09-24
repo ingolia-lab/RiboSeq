13340a0beaa916a88f49ec79f34f8abe44c93446
Nick Ingolia <nick@ingolia.org>
2012-09-24 00:24:33 -0400

    Add Winsorizing to the length distribution scoring

--- src/BedFeatureLDist.hs
+++ src/BedFeatureLDist.hs
@@ -23,6 +23,7 @@ import qualified Data.Iteratee.ListLike as IterLL
 import qualified Data.Vector as V
 import qualified Data.Vector.Unboxed as U
 import qualified Data.Vector.Unboxed.Mutable as UM
+import Statistics.Quantile
 
 import qualified Bio.SamTools.Bam as Bam
 import qualified Bio.SamTools.BamIndex as BamIdx
@@ -59,20 +60,37 @@ doLengthDist bamfile conf = do maybe (return ()) doDist $ confDistOut conf
           withFile cfout WriteMode $ \hout -> 
           let cfIter = Bed.bedTranscriptEnum $ IterLL.mapM_ compareDist
               compareDist trx = let name = BS.unpack . unSeqLabel . geneId $ trx
-                                in do ct <- newCount
-                                      countTrxReads hidx ct trx
-                                      U.freeze ct >>= hPutStrLn hout . compareLine cf name
+                                in do lp <- countTrxReads hidx trx
+                                      hPutStrLn hout $ compareLine cf name lp
           in IterIO.fileDriver cfIter bed
-        doCompareBam cf cfout = do ct <- countAllReads bamfile
-                                   writeFile cfout $ unlines [ compareLine cf bamfile ct ]
+        doCompareBam _cf _cfout = undefined
+--        doCompareBam cf cfout = do ct <- countAllReads bamfile
+--                                   writeFile cfout $ unlines [ compareLine cf bamfile ct ]
 
-compareLine :: U.Vector Int -> String -> U.Vector Int -> String
-compareLine cf trx ctvec = intercalate "\t" [ trx
-                                            , showFFloat (Just 4) score ""
-                                            , show $ U.sum ctvec
-                                            , showFFloat (Just 4) (distribL2 cf ctvec) ""
-                                            ]
-  where score = distribL1 cf ctvec
+compareLine :: U.Vector Int -> String -> LenProf -> String
+compareLine cf trx lp = intercalate "\t" [ trx
+                                         , showFFloat (Just 4) score ""
+                                         , show $ lpTotal lp
+                                         , showFFloat (Just 4) scorel2 ""
+                                         , showFFloat (Just 2) v95 ""
+                                         , showFFloat (Just 1) winsTotal ""
+                                         , showFFloat (Just 4) winsScore ""
+                                         , showFFloat (Just 4) winsRatio ""
+                                         ]
+  where cfdist = U.map fromIntegral cf
+        lpdist = U.map fromIntegral . lpFlatten $ lp
+        score = distribL1 cfdist lpdist
+        scorel2 = distribL2 cfdist lpdist
+        v95 = continuousBy medianUnbiased 19  20 xprof
+        v98 = continuousBy medianUnbiased 49  50 xprof
+        v99 = continuousBy medianUnbiased 99 100 xprof
+        xprof = case lpProfile lp of
+          lprof | V.null lprof -> V.singleton 0.0           
+                | otherwise -> V.map fromIntegral lprof
+        wdist = lpWinsorDist lp (max v95 1.0)
+        winsTotal = U.sum wdist
+        winsScore = distribL1 cfdist wdist
+        winsRatio = winsTotal / (fromIntegral $ lpTotal lp)
 
 countAllReads :: FilePath -> IO (U.Vector Int)
 countAllReads bamfile = do ct <- newCount
@@ -83,28 +101,45 @@ countAllReads bamfile = do ct <- newCount
           Nothing -> return ()
 
 countBedReads :: FilePath -> FilePath -> IO (U.Vector Int)
-countBedReads bedfile bamfile = BamIdx.withIndex bamfile $ \hidx -> do
-  ct <- newCount
-  let trxIter = Bed.bedTranscriptEnum . IterLL.mapM_ $ countTrxReads hidx ct 
-  IterIO.fileDriver trxIter bedfile
-  U.freeze ct
+countBedReads bedfile bamfile = BamIdx.withIndex bamfile $ \hidx -> 
+  let addTrx ct0 trx = do lp <- countTrxReads hidx trx
+                          return $! U.zipWith (+) ct0 (lpFlatten lp)
+      trxIter = Bed.bedTranscriptEnum $ IterLL.foldM addTrx (U.replicate (initlen+1) 0) 
+  in IterIO.fileDriver trxIter bedfile
 
-countTrxReads :: BamIdx.IdxHandle -> UM.IOVector Int -> Transcript -> IO ()
-countTrxReads hidx ct trx = maybe noTarget withTarget $ Bam.lookupTarget header $ unSeqLabel trxref
+countTrxReads :: BamIdx.IdxHandle -> Transcript -> IO LenProf
+countTrxReads hidx trx = maybe noTarget withTarget $ Bam.lookupTarget header $ unSeqLabel trxref
   where header = BamIdx.idxHeader hidx
         (OnSeq trxref trxsploc) = location trx
         trxbnds = (fromIntegral *** fromIntegral) $ Loc.bounds trxsploc
-        noTarget = when verbose $ hPutStrLn stderr . unwords $ [ "Skipping", show . unSeqLabel . geneId $ trx
-                                                               , "on", show . unSeqLabel $ trxref ]
+        noTarget = do when verbose $ hPutStrLn stderr . unwords $ [ "Skipping", show . unSeqLabel . geneId $ trx
+                                                                  , "on", show . unSeqLabel $ trxref ]
+                      return $ LenProf V.empty
         withTarget tid = do when verbose $ hPutStrLn stderr . unwords $ [ "Running", show . unSeqLabel . geneId $ trx ]
+                            lprof <- newLenProf (fromIntegral . Loc.length $ trxsploc)
+                            let countReads = IterLL.mapM_ $ countRead' lprof trxsploc
+                                targetIter = IterLL.joinIM $ BamIter.enumIndexRegion hidx tid trxbnds countReads
                             IterLL.run targetIter
-          where targetIter = IterLL.joinIM $ BamIter.enumIndexRegion hidx tid trxbnds countReads
-        countReads = IterLL.mapM_ $ countRead ct trxsploc
+                            lpFreeze lprof
                      
 countRead :: UM.IOVector Int -> SpLoc.SpliceLoc -> Bam.Bam1 -> IO ()
 countRead ct trxsploc b = case Bam.refSpLoc b >>= flip SpLoc.locInto trxsploc of
   Just into | Loc.strand into == Plus -> countOne ct (fromIntegral $ Loc.length into)
   _ -> return ()
+
+countRead' :: (MonadIO m) => LenProfIO -> SpLoc.SpliceLoc -> Bam.Bam1 -> m ()
+countRead' lprof trxsploc b = case Bam.refSpLoc b >>= flip SpLoc.locInto trxsploc of
+  Just into -> case Loc.startPos into of
+    (Pos.Pos off Plus) -> countOne' lprof off (fromIntegral $ Loc.length into)
+    _ -> return ()
+  _ -> return ()
+
+countOne' :: (MonadIO m) => LenProfIO -> Pos.Offset -> Int -> m ()
+countOne' (LenProf v) off len = liftIO $ do n0 <- UM.read vct len'
+                                            UM.write vct len' $! succ n0
+  where len' = min len (UM.length vct - 1)
+        vct = v V.! off'
+        off' = max 0 $ min (fromIntegral off) (V.length v - 1)
 
 newCount :: (MonadIO m) => m (UM.IOVector Int)
 newCount = liftIO $ UM.replicate (initlen + 1) 0
@@ -136,26 +171,60 @@ parseCountTable = U.fromList . zipWith parseCount [0..] . BS.lines
                                                  else bad
                              _ -> bad
           
-distribL1 :: U.Vector Int -> U.Vector Int -> Double
+distribL1 :: U.Vector Double -> U.Vector Double -> Double
 distribL1 d1 d2 = (* 0.5) $ U.sum $ U.zipWith absDiff growD1 growD2
   where maxlen = max (U.length d1) (U.length d2)
         growD1 = (toDistrib d1) U.++ (U.replicate (maxlen - U.length d1) 0.0)
         growD2 = (toDistrib d2) U.++ (U.replicate (maxlen - U.length d2) 0.0)
-        toDistrib d = U.map ((/ total) . fromIntegral) d
-          where total = fromIntegral $ U.sum d
+        toDistrib d = U.map (/ total) d
+          where total = U.sum d
         absDiff x1 x2 = abs $ x1 - x2
 
-distribL2 :: U.Vector Int -> U.Vector Int -> Double
+distribL2 :: U.Vector Double -> U.Vector Double -> Double
 distribL2 d1 d2 = sqrt . U.sum . U.map (^ 2) $ U.zipWith (-) growD1 growD2
   where maxlen = max (U.length d1) (U.length d2)
         growD1 = (toDistrib d1) U.++ (U.replicate (maxlen - U.length d1) 0.0)
         growD2 = (toDistrib d2) U.++ (U.replicate (maxlen - U.length d2) 0.0)
-        toDistrib d = U.map ((/ total) . fromIntegral) d
-          where total = fromIntegral $ U.sum d
+        toDistrib d = U.map (/ total) d
+          where total = U.sum d
+
+newtype LenProfBase v e = LenProf { unLenProf :: V.Vector (v e) }
+type LenProf = LenProfBase U.Vector Int
+type LenProfIO = LenProfBase UM.IOVector Int
+
+initlen :: Int
+initlen = 51
+
+zerodist :: (U.Unbox a, Num a) => U.Vector a
+zerodist = U.replicate (initlen + 1) 0
+
+newLenProf :: (MonadIO m) => Int -> m LenProfIO
+newLenProf l = liftIO . liftM LenProf . V.replicateM l . UM.replicate (initlen + 1) $ 0
+
+lpFreeze :: (MonadIO m) => LenProfIO -> m LenProf
+lpFreeze = liftM LenProf . liftIO . V.mapM U.freeze . unLenProf
+
+lpTotal :: LenProf -> Int
+lpTotal = V.sum . V.map U.sum . unLenProf
+
+lpFlatten :: LenProf -> U.Vector Int
+lpFlatten (LenProf v) = V.foldl' (U.zipWith (+)) zerodist v
+
+lpProfile :: LenProf -> V.Vector Int
+lpProfile = V.map U.sum . unLenProf
+
+lpWinsorDist :: LenProf -> Double -> U.Vector Double
+lpWinsorDist (LenProf v) w = V.foldl' (U.zipWith (+)) zerodist 
+                             . V.map winsor $ v
+  where winsor zdist = let xdist = U.map fromIntegral zdist
+                       in case U.sum xdist of
+                         xttl | xttl > w -> U.map (* (w / xttl)) xdist
+                              | otherwise -> xdist
 
 data Conf = Conf { confDistOut :: !(Maybe FilePath)
                  , confCompare :: !(Maybe (FilePath, FilePath))
                  , confBedFile :: !(Maybe FilePath)
+                 , confJackpot :: !(Maybe Double)
                  } deriving (Show)
 
 confCompareDistFile :: Conf -> Maybe FilePath
@@ -172,6 +241,7 @@ data Arg = ArgDistOut { unArgDistOut :: !String }
          | ArgCompareDist { unArgCompareDist :: !String }
          | ArgCompareScore { unArgCompareScore :: !String }
          | ArgBedFile { unArgBedFile :: !String }         
+         | ArgJackpot { unArgJackpot :: !String }
          deriving (Show, Read, Eq, Ord)
 
 argDistOut :: Arg -> Maybe String
@@ -190,11 +260,16 @@ argBedFile :: Arg -> Maybe String
 argBedFile (ArgBedFile bed) = Just bed
 argBedFile _ = Nothing
 
+argJackpot :: Arg -> Maybe String
+argJackpot (ArgJackpot jp) = Just jp
+argJackpot _ = Nothing
+
 optDescrs :: [OptDescr Arg]
 optDescrs = [ Option ['o'] ["output"]     (ReqArg ArgDistOut "OUTFILE")       "Length distribution output filename"
             , Option ['c'] ["compare"]    (ReqArg ArgCompareDist "DISTFILE")  "Length distribution comparison filename"
             , Option ['b'] ["bed"]        (ReqArg ArgBedFile "BEDFILE")       "Transcript bed filename"
             , Option ['s'] ["scores"]     (ReqArg ArgCompareScore "SCOREFILE") "Per-transcript comparison output filename"
+            , Option ['j'] ["jackpot"]    (ReqArg ArgJackpot "Q")             "Read count quantile for jackpot suppression"
             ]
 
 argsToConf :: [Arg] -> Either String Conf
@@ -202,7 +277,8 @@ argsToConf = runReaderT conf
     where conf = Conf <$> 
                  findDistOut <*>
                  findCompare <*>
-                 findBed
+                 findBed <*>
+                 findJackpot
           findDistOut = ReaderT $ return . listToMaybe . mapMaybe argDistOut
           findBed = ReaderT $ return . listToMaybe . mapMaybe argBedFile
           findCompareDist = ReaderT $ return . listToMaybe . mapMaybe argCompareDist
@@ -214,7 +290,8 @@ argsToConf = runReaderT conf
                              (Nothing, Nothing) -> return Nothing
                              (Just dist, Nothing) -> lift $ Left "No comparison output filename"
                              (Nothing, Just score) -> lift $ Left "No comparison distribution given"
-
+          findJackpot = ReaderT $ maybe (return Nothing) (liftM Just . parseJackpot) . listToMaybe . mapMaybe argJackpot
+          parseJackpot = AP.parseOnly AP.double . BS.pack
 
 verbose :: Bool
 verbose = False
