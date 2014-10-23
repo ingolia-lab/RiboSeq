778ea234fa714ed772013b9ac302c0ec48df3e05
Nicholas Ingolia <nick@ingolia.org>
2014-10-23 14:17:30 -0700

    Added counting Refactored a separate module

---
+++ src/Bio/RiboSeq/LinkerSeqs.hs
@@ -0,0 +1,109 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+module Bio.RiboSeq.LinkerSeqs
+       where
+
+import qualified Data.ByteString.Char8 as BS
+import Data.Either
+import Data.List
+import qualified Data.Vector.Unboxed.Mutable as UM
+import System.IO
+
+--
+data FastQ = FQ { fqname, fqseq, fqqual :: !BS.ByteString } deriving (Show, Read)
+
+hWriteFq :: Handle -> FastQ -> IO ()
+hWriteFq h fq = BS.hPutStr h $ BS.unlines [ '@' `BS.cons` fqname fq, fqseq fq, "+", fqqual fq ]
+
+--
+data FormatNt = Prefix !Int | Suffix !Int deriving (Show, Read, Eq)
+
+data LinkerFormat = LinkerFormat { prefixLength :: !Int,
+                                   suffixLength :: !Int,
+                                   sampleIndex :: ![FormatNt],
+                                   seqBarcode :: ![FormatNt] } deriving (Show, Read, Eq)
+
+seqBarcodeIdxlen :: LinkerFormat -> Int
+seqBarcodeIdxlen = lenIndex . length . seqBarcode
+
+sampleIndexIdxlen :: LinkerFormat -> Int
+sampleIndexIdxlen = lenIndex . length . sampleIndex
+
+parseLinkerFormat :: String -> String -> LinkerFormat
+parseLinkerFormat pfx sfx = LinkerFormat { prefixLength = length pfx
+                                         , suffixLength = length sfx
+                                         , sampleIndex = idxs
+                                         , seqBarcode = bcds }
+  where (idxs, bcds) = partitionEithers $ unfoldr fmt (zip [0..] pfx, zip [0..] sfx)
+        fmt ([], []) = Nothing
+        fmt ([], ((sidx, s):srest)) | s == 'I' = Just (Left  (Suffix sidx), ([], srest))
+                                    | s == 'N' = Just (Right (Suffix sidx), ([], srest))
+                                    | otherwise = error $ "Unknown linker format specifier " ++ show s ++ " in " ++ show (pfx, sfx)
+        fmt (((pidx, p):prest), ss) | p == 'I' = Just (Left  (Prefix pidx), (prest, ss))
+                                    | p == 'N' = Just (Right (Prefix pidx), (prest, ss))
+                                    | otherwise = error $ "Unknown linker format specifier " ++ show p ++ " in " ++ show (pfx, sfx)
+
+--unparseLinkerFormat :: LinkerFormat -> (String, String)
+--unparseLinkerFormat lf = 
+
+--
+data LinkerRes = TooShort
+               | Res { sequEnce, sequIndex, sequBarcode, sequQual :: !BS.ByteString }
+               deriving (Show, Read)
+
+sequLinkers :: LinkerFormat -> FastQ -> LinkerRes
+sequLinkers lfmt fq | BS.length (fqseq fq) < (prefixLength lfmt + suffixLength lfmt) = TooShort
+                    | otherwise = let (pfx, rest) = BS.splitAt (prefixLength lfmt) $ fqseq fq
+                                      (ence, sfx) = BS.splitAt (BS.length rest - suffixLength lfmt) rest
+                                      qual = BS.take (BS.length ence) . BS.drop (prefixLength lfmt) $ fqqual fq
+                                    in Res { sequEnce = ence
+                                           , sequIndex = unlinkerize pfx sfx $ sampleIndex lfmt
+                                           , sequBarcode = unlinkerize pfx sfx $ seqBarcode lfmt
+                                           , sequQual = qual
+                                           }
+                                       
+unlinkerize :: BS.ByteString -> BS.ByteString -> [FormatNt] -> BS.ByteString
+unlinkerize pfx sfx = BS.pack . map unlinkerChar
+  where unlinkerChar (Prefix i) = pfx `BS.index` i
+        unlinkerChar (Suffix i) = sfx `BS.index` i
+
+--
+newtype Index = Index { unIndex :: Int } deriving (Show, Read, Eq, Ord, Num)
+
+mismatches :: BS.ByteString -> [BS.ByteString]
+mismatches s = concatMap mmat [0..(BS.length s - 1)]
+  where mmat i = let before = BS.take i s
+                     after = BS.drop (i + 1) s
+                 in [ BS.concat [ before, BS.singleton ch, after] | ch <- "ACGT", ch /= BS.index s i ]
+
+toIndex :: BS.ByteString -> Index
+toIndex = BS.foldl' idxch 0
+  where idxch i0 ch = let !i' = i0 * 4
+                          !ich = case ch of
+                                  'a' -> 0
+                                  'A' -> 0
+                                  'c' -> 1
+                                  'C' -> 1
+                                  'g' -> 2
+                                  'G' -> 2
+                                  't' -> 3
+                                  'T' -> 3
+                                  _ -> error $ "toIndex: Bad nucleotide " ++ show ch
+                      in i' + ich
+
+maxIndex :: Int -> Index
+maxIndex len = (4 ^ len) - 1
+
+lenIndex :: Int -> Int
+lenIndex len = 4 ^ len
+
+countSeq :: UM.IOVector Int -> BS.ByteString -> IO ()
+countSeq ctvec sequ | UM.length ctvec >= lenIndex (BS.length sequ)
+                      = let idx = toIndex sequ
+                        in UM.read ctvec (unIndex idx) >>= UM.write ctvec (unIndex idx) . (succ $!)
+                    | otherwise = fail $ "Sequence " ++ show sequ ++ " length " ++ show (BS.length sequ) ++ " needs "
+                                  ++ (show . lenIndex . BS.length $ sequ) ++ " but count vector length " ++ show (UM.length ctvec)
--- src/FastxSplit.hs
+++ src/FastxSplit.hs
@@ -32,6 +32,8 @@ import qualified Data.Conduit as C
 import qualified Data.Conduit.Binary as CB
 import qualified Data.Conduit.List as C
 
+import Bio.RiboSeq.LinkerSeqs
+
 main :: IO ()
 main = run ( fxs, info )
   where info = defTI { termName = "fastx-split"
@@ -89,16 +91,14 @@ handleSeq conf sfate fq = do
         sample res s = seqToSample conf s fq res
 
 seqToSample :: Conf -> Sample -> FastQ -> LinkerRes -> IO ()
-seqToSample _conf s fq0 res = hWriteFq (sHandle s) fq'
+seqToSample _conf s fq0 res = do hWriteFq (sHandle s) fq'
+                                 countSeq (sIndexes s) (sequIndex res)
+                                 countSeq (sBarcodes s) (sequBarcode res)
+                                 
   where fq' = FQ { fqname = BS.unwords [ fqname fq0, sequIndex res, sequBarcode res ]
                  , fqseq  = sequEnce res
                  , fqqual = sequQual res
                  }
-
-data FastQ = FQ { fqname, fqseq, fqqual :: !BS.ByteString } deriving (Show, Read)
-
-hWriteFq :: Handle -> FastQ -> IO ()
-hWriteFq h fq = BS.hPutStr h $ BS.unlines [ '@' `BS.cons` fqname fq, fqseq fq, "+", fqqual fq ]
 
 readSampleSheet :: (MonadIO m) => Conf -> m [SampleSpec]
 readSampleSheet conf = (liftIO . BS.readFile . cSampleSheet $ conf) >>= parseSampleSheet conf
@@ -163,35 +163,6 @@ data SampleFate = Fate { sfMap :: !(HM.HashMap BS.ByteString Sample)
                        , sfCount :: !(IORef Int)
                        }
 
-newtype Index = Index { unIndex :: Int } deriving (Show, Read, Eq, Ord, Num)
-
-mismatches :: BS.ByteString -> [BS.ByteString]
-mismatches s = concatMap mmat [0..(BS.length s - 1)]
-  where mmat i = let before = BS.take i s
-                     after = BS.drop (i + 1) s
-                 in [ BS.concat [ before, BS.singleton ch, after] | ch <- "ACGT", ch /= BS.index s i ]
-
-toIndex :: BS.ByteString -> Index
-toIndex = BS.foldl' idxch 0
-  where idxch i0 ch = let !i' = i0 * 4
-                          !ich = case ch of
-                                  'a' -> 0
-                                  'A' -> 0
-                                  'c' -> 1
-                                  'C' -> 1
-                                  'g' -> 2
-                                  'G' -> 2
-                                  't' -> 3
-                                  'T' -> 3
-                                  _ -> error $ "toIndex: Bad nucleotide " ++ show ch
-                      in i' + ich
-
-maxIndex :: Int -> Index
-maxIndex len = (4 ^ len) - 1
-
-lenIndex :: Int -> Int
-lenIndex len = 4 ^ len
-
 data SampleSpec = SampleSpec { sName :: !BS.ByteString, sIndex :: !BS.ByteString } deriving (Show, Read)
 
 data Sample = Sample { sSpec :: !SampleSpec,
@@ -210,53 +181,6 @@ data Conf = Conf { cInputs :: ![FilePath],
                    cSampleSheet :: !FilePath,
                    cPerfectOnly :: !Bool
                  } deriving (Read, Show)
-
-data FormatNt = Prefix !Int | Suffix !Int deriving (Show, Read, Eq)
-
-data LinkerFormat = LinkerFormat { prefixLength :: !Int,
-                                   suffixLength :: !Int,
-                                   sampleIndex :: ![FormatNt],
-                                   seqBarcode :: ![FormatNt] } deriving (Show, Read, Eq)
-
-seqBarcodeIdxlen :: LinkerFormat -> Int
-seqBarcodeIdxlen = lenIndex . length . seqBarcode
-
-sampleIndexIdxlen :: LinkerFormat -> Int
-sampleIndexIdxlen = lenIndex . length . sampleIndex
-
-data LinkerRes = TooShort
-               | Res { sequEnce, sequIndex, sequBarcode, sequQual :: !BS.ByteString }
-               deriving (Show, Read)
-
-sequLinkers :: LinkerFormat -> FastQ -> LinkerRes
-sequLinkers lfmt fq | BS.length (fqseq fq) < (prefixLength lfmt + suffixLength lfmt) = TooShort
-                    | otherwise = let (pfx, rest) = BS.splitAt (prefixLength lfmt) $ fqseq fq
-                                      (ence, sfx) = BS.splitAt (BS.length rest - suffixLength lfmt) rest
-                                      qual = BS.take (BS.length ence) . BS.drop (prefixLength lfmt) $ fqqual fq
-                                    in Res { sequEnce = ence
-                                           , sequIndex = unlinkerize pfx sfx $ sampleIndex lfmt
-                                           , sequBarcode = unlinkerize pfx sfx $ seqBarcode lfmt
-                                           , sequQual = qual
-                                           }
-                                       
-unlinkerize :: BS.ByteString -> BS.ByteString -> [FormatNt] -> BS.ByteString
-unlinkerize pfx sfx = BS.pack . map unlinkerChar
-  where unlinkerChar (Prefix i) = pfx `BS.index` i
-        unlinkerChar (Suffix i) = sfx `BS.index` i
-
-parseLinkerFormat :: String -> String -> LinkerFormat
-parseLinkerFormat pfx sfx = LinkerFormat { prefixLength = length pfx
-                                         , suffixLength = length sfx
-                                         , sampleIndex = idxs
-                                         , seqBarcode = bcds }
-  where (idxs, bcds) = partitionEithers $ unfoldr fmt (zip [0..] pfx, zip [0..] sfx)
-        fmt ([], []) = Nothing
-        fmt ([], ((sidx, s):srest)) | s == 'I' = Just (Left  (Suffix sidx), ([], srest))
-                                    | s == 'N' = Just (Right (Suffix sidx), ([], srest))
-                                    | otherwise = error $ "Unknown linker format specifier " ++ show s ++ " in " ++ show (pfx, sfx)
-        fmt (((pidx, p):prest), ss) | p == 'I' = Just (Left  (Prefix pidx), (prest, ss))
-                                    | p == 'N' = Just (Right (Prefix pidx), (prest, ss))
-                                    | otherwise = error $ "Unknown linker format specifier " ++ show p ++ " in " ++ show (pfx, sfx)
 
 cOutFile :: Conf -> FilePath -> FilePath
 cOutFile conf = ((cOutDir conf) </>)
