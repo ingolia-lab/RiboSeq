47b2cfd0308b071ccad74ac3cb48530fb3ea88a2
Nick Ingolia <nick@ingolia.org>
2015-02-22 16:28:22 -0800

    New infrstructure for counting reads for QC framing

--- src/Bio/RiboSeq/Framing.hs
+++ src/Bio/RiboSeq/Framing.hs
@@ -18,10 +18,15 @@ import Data.Maybe
 import Data.Ord
 import qualified Data.Vector as V
 import qualified Data.Vector.Mutable as VM
+import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector.Unboxed.Mutable as UM
 
 import qualified Bio.SamTools.Bam as Bam
 import qualified Bio.SeqLoc.Location as Loc
+import Bio.SeqLoc.OnSeq
 import qualified Bio.SeqLoc.Position as Pos
+import qualified Bio.SeqLoc.SpliceLocation as SpLoc
+import Bio.SeqLoc.Strand
 import Bio.SeqLoc.Transcript
 
 import Bio.RiboSeq.BamFile
@@ -132,3 +137,99 @@ count2D :: V.Vector (VM.IOVector Int) -> Int -> Int -> IO ()
 count2D v i1 i2 = do x <- VM.read (v V.! i1) i2
                      VM.write (v V.! i1) i2 $! succ x
                      
+data LengthFrameIO = LengthFrameIO { lfioMinLen :: !Int
+                                   , lfioMinPos :: !Pos.Offset
+                                   , lfioProfile :: !(VM.IOVector (UM.IOVector Int))
+                                   }
+
+lfioFreeze :: LengthFrameIO -> IO (LengthFrame)
+lfioFreeze lfio = LengthFrame <$>
+                  pure (lfioMinLen lfio) <*>
+                  pure (lfioMinPos lfio) <*>
+                  ((V.freeze . lfioProfile $ lfio) >>= V.mapM U.freeze)
+
+lfioNew :: (Pos.Offset, Pos.Offset) -> (Int, Int) -> IO LengthFrameIO
+lfioNew (minpos, maxpos) (minlen, maxlen)
+  = LengthFrameIO <$>
+    pure minlen <*>
+    pure minpos <*>
+    VM.replicateM (fromIntegral $ 1 + maxpos - minpos) (UM.replicate (1 + maxlen - minlen) 0)
+
+lfioIncr :: LengthFrameIO -> Pos.Offset -> Int -> IO Bool
+lfioIncr lfio pos len = if posidx >= 0 && posidx < (VM.length . lfioProfile $ lfio)
+                        then do lprof <- VM.read (lfioProfile lfio) posidx
+                                if lenidx >= 0 && lenidx < (UM.length lprof)
+                                  then do n0 <- UM.read lprof lenidx
+                                          UM.write lprof lenidx $! succ n0
+                                          return True
+                                  else return False
+                        else return False
+  where posidx = fromIntegral $ pos - (lfioMinPos lfio)
+        lenidx = len - (lfioMinLen lfio)
+
+data LengthFrame = LengthFrame { lfMinLen :: !Int
+                               , lfMinPos :: !Pos.Offset
+                               , lfProfile :: !(V.Vector (U.Vector Int))
+                               } deriving (Show)
+                   
+data FramingStatsIO = FramingStatsIO { fsioStart, fsioEnd, fsioBody :: !LengthFrameIO
+                                     , fsioBodyStartMin :: Pos.Offset
+                                     , fsioBodyEndMax :: Pos.Offset
+                                     }
+
+data FramingStats = FramingStats { fsStart, fsEnd, fsBody :: LengthFrame
+                                 , fsBodyStartMin :: Pos.Offset
+                                 , fsBodyEndMax :: Pos.Offset
+                                 } deriving (Show)
+
+fsioNew :: (Pos.Offset, Pos.Offset) ->
+           (Pos.Offset, Pos.Offset) ->
+           Pos.Offset -> Pos.Offset ->
+           (Int, Int) ->
+           IO FramingStatsIO
+fsioNew startBnds endBnds bodyStartMin bodyEndMax lenBnds =
+  FramingStatsIO <$>
+  lfioNew startBnds lenBnds <*>
+  lfioNew endBnds lenBnds <*>
+  lfioNew frameBnds lenBnds <*>
+  (pure bodyStartMin) <*>
+  (pure bodyEndMax)
+  where frameBnds = (0, 2)
+
+fsioFreeze :: FramingStatsIO -> IO FramingStats
+fsioFreeze fsio = FramingStats <$>
+                  (lfioFreeze . fsioStart $ fsio) <*>
+                  (lfioFreeze . fsioEnd $ fsio) <*>
+                  (lfioFreeze . fsioBody $ fsio) <*>
+                  (pure . fsioBodyStartMin $ fsio) <*>
+                  (pure . fsioBodyEndMax $ fsio)
+
+fsioIncr :: FramingStatsIO -> (Pos.Offset, Pos.Offset) -> Int -> IO (Bool, Bool, Bool)
+fsioIncr fsio (vsStart, vsEnd) len = do
+  atStart <- lfioIncr (fsioStart fsio) vsStart len
+  atEnd <- lfioIncr (fsioEnd fsio) vsEnd len
+  inBody <- if (vsStart >= fsioBodyStartMin fsio) && (vsEnd <= fsioBodyEndMax fsio)
+            then lfioIncr (fsioBody fsio) (vsStart `mod` 3) len
+            else return False
+  return (atStart, atEnd, inBody)
+
+-- | When the location of @bam@ (as per 'Bam.refSeqLoc') lies within
+-- the location of @trx@ and on the forward strand, and @trx@ has an
+-- annotated CDS, return @Just@ the position of the 5\' end of the hit
+-- relative to the CDS.  The first @Pos.Offset@ is the position
+-- relative to the start codon, with '0' indicating the first
+-- nucleotide of the start codon is the 5\' end of the alignment. The
+-- second @Pos.Offset@ is the position relative to the stop codon,
+-- with '0' indicating that the first nucleotide of the stop codon is
+-- the 5\' end of the alignment.
+bamCdsRel :: Transcript -> Bam.Bam1 -> Maybe (Pos.Offset, Pos.Offset)
+bamCdsRel trx bam = bamRel <$> cds trx <*> (Bam.refSeqLoc bam >>= bamInto (location trx))
+  where bamInto (OnSeq trxRef trxLoc) (OnSeq bamRef bamLoc)
+          | trxRef /= bamRef = Nothing
+          | otherwise = do into <- bamLoc `SpLoc.locInto` trxLoc
+                           case Loc.toContigs into of
+                             [c] | Loc.strand c == Plus -> Just c
+                             _ -> Nothing
+        bamRel cdsLoc bamLoc = ( Loc.offset5 bamLoc - Loc.offset5 cdsLoc
+                               , Loc.offset5 bamLoc - (Loc.offset5 cdsLoc + Loc.length cdsLoc - 3)
+                               ) 
