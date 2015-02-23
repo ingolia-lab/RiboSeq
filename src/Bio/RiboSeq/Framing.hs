b74e7a268b6fd43a2e9639e7e682cc734e9ab93b
Nick Ingolia <nick@ingolia.org>
2015-02-22 20:40:29 -0800

    Calculate framing of Bam alignments
    Documented meaning of various framing results

--- src/Bio/RiboSeq/Framing.hs
+++ src/Bio/RiboSeq/Framing.hs
@@ -217,18 +217,45 @@ fsioFreeze fsio = FramingStats <$>
 -- | When the location of @bam@ (as per 'Bam.refSeqLoc') lies within
 -- the location of @trx@ and on the forward strand, and @trx@ has an
 -- annotated CDS, return @Just@ the position of the 5\' end of the hit
--- relative to the CDS.  The first @Pos.Offset@ is the position
--- relative to the start codon, with '0' indicating the first
--- nucleotide of the start codon is the 5\' end of the alignment. The
--- second @Pos.Offset@ is the position relative to the stop codon,
--- with '0' indicating that the first nucleotide of the stop codon is
--- the 5\' end of the alignment.
+-- relative to the CDS.
+
+data BamFailure = BamNoHit
+                | BamMultiHit
+                | BamFpFailure !FpFailure
+                deriving (Show, Ord, Eq)
+
+instance Bounded BamFailure where
+  minBound = BamNoHit
+  maxBound = BamFpFailure maxBound
+
+instance Enum BamFailure where
+  toEnum 0 = BamNoHit
+  toEnum 1 = BamMultiHit
+  toEnum n | n >= 2 = BamFpFailure (toEnum $ n - 2)
+  fromEnum BamNoHit = 0
+  fromEnum BamMultiHit = 1
+  fromEnum (BamFpFailure fpf) = 2 + fromEnum fpf
+
+type BamFraming = Either BamFailure (Maybe Pos.Offset, Maybe Pos.Offset, Maybe Pos.Offset)
+
+-- | Find the framing of a 'Bam.Bam1' alignment of a footprint. When
+-- the footprint alignment is unique -- i.e., it has a genomic
+-- location as per 'Bam.refSeqLoc' and does not indicate multiple hits
+-- as per 'Bam.nHits' -- then the unique genomic location is used to
+-- determine the framing as per 'fpFraming'.
+bamFraming :: (Pos.Offset, Pos.Offset) -> LM.SeqLocMap Transcript -> Bam.Bam1 -> BamFraming
+bamFraming bodyBnds trxmap bam
+  | multiHit bam = Left BamMultiHit
+  | otherwise = maybe (Left BamNoHit) locFraming . Bam.refSeqLoc $ bam
+  where multiHit = maybe False (> 1) . Bam.nHits
+        locFraming = either (Left . BamFpFailure) Right . fpFraming bodyBnds trxmap
 
 data FpFailure = FpNoGene
                | FpNoncodingOnly
                | FpNoncodingOverlap
                | FpMultiCoding
                | FpNoCompatible
+               | FpAmbigFrame
                deriving (Show, Ord, Eq, Bounded, Enum)
 
 type FpFraming = Either FpFailure (Maybe Pos.Offset, Maybe Pos.Offset, Maybe Pos.Offset)
@@ -243,40 +270,102 @@ isNoncoding = isNothing . cds
 isCoding :: Transcript -> Bool
 isCoding = isJust . cds
 
-maybeAllSame :: (Eq a) => [a] -> Maybe a
-maybeAllSame [] = Nothing
-maybeAllSame (x0:rest) = if all (== x0) rest then Just x0 else Nothing
-
-fpFramings :: (Pos.Offset, Pos.Offset) -> LM.SeqLocMap Transcript -> SpliceSeqLoc -> FpFraming
-fpFramings bodyBnds trxmap fploc = case groupByGene $ LM.queryLocatable (Just Plus) fploc trxmap of
+-- | Find the framing of an @fpLoc@ specified in /genomic/ coordinates
+-- relative to the 'Transcript' genome annotations that it intersects.
+--
+-- The full set of 'Transcript' objects whose extent overlaps with the
+-- query @fpLoc@, as per 'LM.queryLocatable', is considered.
+--
+-- When the footprint doesn't overlap the extent of any transcript,
+-- then 'FpNoGene' is returned. Otherwise, transcripts are grouped by
+-- their 'geneId' values in order to analyze the gene(s) overlapping
+-- the query footprint location. Multiple distinct genes yield
+-- 'FpNoncodingOnly' (if all are non-coding, i.e., lack any
+-- 'Transcript' with a 'cds'), 'FpNoncodingOverlap' (for a mix of
+-- non-coding and coding genes), or 'FpMultiCoding' (for multiple
+-- genes all coding). A single, non-coding gene hit also yields
+-- 'FpNoncodingOnly'.
+--
+-- When all 'Transcript' objects overlapping the query @fpLoc@ share
+-- the same 'geneId' and at least one of them is coding (i.e., has a
+-- 'cds') then the framing is computed as per 'geneFraming'.
+fpFraming :: (Pos.Offset, Pos.Offset) -> LM.SeqLocMap Transcript -> SpliceSeqLoc -> FpFraming
+fpFraming bodyBnds trxmap fploc = case groupByGene $ LM.queryLocatable (Just Plus) fploc trxmap of
   [] -> Left FpNoGene
   [trxs] -> case filter isCoding trxs of
     [] -> Left FpNoncodingOnly
-    codings -> geneFramings bodyBnds codings fploc
+    codings -> geneFraming bodyBnds codings fploc
   genes -> case map (all isNoncoding) genes of
     isnc | and isnc  -> Left FpNoncodingOnly
          | or isnc   -> Left FpNoncodingOverlap
          | otherwise -> Left FpMultiCoding
 
-geneFramings :: (Pos.Offset, Pos.Offset) -> [Transcript] -> SpliceSeqLoc -> FpFraming
-geneFramings (bodyStartMin, bodyEndMax) codings fploc =
+-- | Find the framing of an @fpLoc@ specified in /genomic/ coordinates
+-- relative to a collection of 'Transcript' objects derived from a
+-- single gene.
+--
+-- For each individual 'Transcript' the start- and stop-relative
+-- offsets are computed according to 'cdsRelIntoTranscript'. When
+-- these offsets are not defined, typically because the @fpLoc@ is
+-- incompatible with the splice structure of a specific 'Transcript',
+-- then they are dropped from further consideration.
+--
+-- If no 'Transcript' has defined offsets, then 'FpNoCompatible' is
+-- returned.
+--
+-- Reading frame positions are computed when the start-relative offset
+-- is defined and no smaller than @bodyStartMin@, and the
+-- stop-relative offset is defined and no larger than
+-- @bodyEndMax. When more than one reading frame position is defined
+-- -- i.e., there are two sets of start- and stop-relative offsets
+-- falling within the specified range of offsets for the \"body\" of
+-- the gene that would indicate different reading frames for @fpLoc@
+-- -- then 'FpAmbigFrame' is returned.
+--
+-- When at least one 'Transcript' has a compatible structure with
+-- @fpLoc@ and there are no conflicting reading frames within the
+-- "body" of the CDS, then a successful 'FpFraming' is returned. The
+-- start-relative offset is defined (i.e., not 'Nothing') when it is
+-- the same across all valid transcripts, and likewise for the
+-- stop-relative offset. The reading frame is defined when @fpLoc@
+-- lies within the body of at least one transcript, and 'Nothing' if
+-- it is not the body of any transcript; note that ambiguity in the
+-- reading frame leads to an 'FpAmbigFrame' failure.
+geneFraming :: (Pos.Offset, Pos.Offset) -> [Transcript] -> SpliceSeqLoc -> FpFraming
+geneFraming (bodyStartMin, bodyEndMax) codings fploc =
   case mapMaybe (cdsRelIntoTranscript fploc) codings of
     [] -> Left FpNoCompatible
-    termini -> let (vsStarts, vsEnds) = unzip termini
+    termini -> let vsStart = maybeAllSame . map fst $ termini
+                   vsEnd = maybeAllSame . map snd $ termini
                    frames = mapMaybe cdsRelToFrame termini
-               in Right ( maybeAllSame vsStarts
-                        , maybeAllSame vsEnds
-                        , maybeAllSame frames
-                        )
+               in case group frames of
+                 [] -> Right ( vsStart, vsEnd, Nothing )
+                 [(fr:_)] -> Right ( vsStart, vsEnd, Just fr )
+                 _ -> Left FpAmbigFrame
   where cdsRelToFrame (vsStart, vsEnd)
           | (vsStart >= bodyStartMin) && (vsEnd <= bodyEndMax) = Just $! vsStart `mod` 3
           | otherwise = Nothing
+        maybeAllSame :: (Eq a) => [a] -> Maybe a
+        maybeAllSame [] = Nothing
+        maybeAllSame (x0:rest) = if all (== x0) rest then Just x0 else Nothing
                 
+-- | Find the relative position of an @fpLoc@ specified in /genomic/
+-- coordinates relative to the start and stop codons of the
+-- transcript, as per 'cdsRel', if these are defined. If the
+-- transcript-relative coordiantes of @fpLoc@ are undefined as per
+-- 'spliceSeqLocIntoContig', or the transcript lacks a 'cds', then
+-- 'Nothing' is returned.
 cdsRelIntoTranscript :: SpliceSeqLoc -> Transcript -> Maybe (Pos.Offset, Pos.Offset)  
 cdsRelIntoTranscript fploc trx = do fpinto <- spliceSeqLocIntoContig fploc $ location trx
                                     cdsloc <- cds trx
                                     return $! cdsRel cdsloc fpinto
 
+-- | Find a contiguous mapping of one 'SpliceSeqLoc' as a subset of an
+-- outer 'SpliceSeqLoc' if it exists. This function returns 'Just' a
+-- 'Loc.ContigLoc' that would satisfy @'Loc.clocOutof' into ('unOnSeq'
+-- outer) == 'unOnSeq' loc@. When the reference sequence names don't
+-- match, or when the query location is not a contiguous sublocation
+-- of the outer location, then 'Nothing' is returned.
 spliceSeqLocIntoContig (OnSeq spref sploc) (OnSeq outref outloc)
   | spref /= outref = Nothing
   | otherwise = sploc `SpLoc.locInto` outloc >>= toSingleContig
@@ -284,6 +373,15 @@ spliceSeqLocIntoContig (OnSeq spref sploc) (OnSeq outref outloc)
           [c] -> Just c
           _ -> Nothing
 
+-- | Find the relative position of an @fpLoc@ specified in
+-- /transcript-relative/ coordinates, relative to the start and stop
+-- codons of a @cdsLoc@ (also defined in transcript-relative
+-- coordinates). The first @Pos.Offset@ is the position relative to
+-- the start codon, with '0' indicating the first nucleotide of the
+-- start codon is the 5\' end of the alignment. The second
+-- @Pos.Offset@ is the position relative to the stop codon, with '0'
+-- indicating that the first nucleotide of the stop codon is the 5\'
+-- end of the alignment.
 cdsRel :: Loc.ContigLoc -> Loc.ContigLoc -> (Pos.Offset, Pos.Offset)
 cdsRel cdsLoc fpLoc =
   ( Loc.offset5 fpLoc - Loc.offset5 cdsLoc
